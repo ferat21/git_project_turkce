ca928d294132107dbf0a1ad2787c78e329e290a9
firat ulas <fulas@gmail.com>
2020-11-04 15:50:44 +0100

    ilk komutum

---
+++ src/day01/ikinciClass.java
@@ -0,0 +1,10 @@
+package day01;
+
+public class ikinciClass {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ src/day01/ilkClass.java
@@ -0,0 +1,10 @@
+package day01;
+
+public class ilkClass {
+
+	public static void main(String[] args) {
+		System.out.println("Hamza'dan Selamlar");
+
+	}
+
+}
